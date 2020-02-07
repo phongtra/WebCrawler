aa84cd81d80ff121428e3347e6f30f6d62574d6f
Phong Tran <aphtr001@edu.xamk.fi>
2020-02-07 18:01:52 +0700

    Managed to extract content from the main page.
    Managed to extract

--- Crawler/Program.cs
+++ Crawler/Program.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
+using AngleSharp;
+using AngleSharp.Dom;
+using AngleSharp.Html.Dom;
+using AngleSharp.Html.Parser;
 using Content.Domain.Models;
 using PuppeteerSharp;
 
@@ -13,24 +18,79 @@ namespace Crawler
         {
             //Download chromium browser revision package
             await new BrowserFetcher().DownloadAsync(ChromiumRevision);
-            string pageContent;
+            string mainPageContent;
+            string kinhDoanhPageContent;
             using (var browser = await Puppeteer.LaunchAsync(new LaunchOptions
             {
                 // Headless = true
                 Headless = false
             }))
             {
-                var url = "http://vnexpress.net";
+                var url = "https://vnexpress.net/";
+                var page = await browser.NewPageAsync();
+                mainPageContent = await GetPageContent(page, url, 1024, 768);
+                var element = "//nav[@id='main_menu']/a[contains(., 'Kinh doanh')]";
+                await Navigate(page, element);
+                
+                kinhDoanhPageContent = await GetPageContent(page, page.Url, 1024, 768);
+                ExtractKinhDoanhPage(kinhDoanhPageContent, ".container .sidebar_1 .list_news", page);
 
-                pageContent = await GetPageContent(browser, url, 1024, 768);
-                var pageModel = CreatePageModel(url, pageContent);
-
+                
+                var pageModel = CreatePageModel(url, mainPageContent);
+                await page.CloseAsync();
                 //Close headless browser, all pages will be closed here.
                 await browser.CloseAsync();
 
             }
 
-            Console.WriteLine(pageContent);
+            
+        }
+
+        private async static void ExtractKinhDoanhPage(string kinhDoanhPageContent, string selector, Page page)
+        {
+            var context = BrowsingContext.New(Configuration.Default);
+            var parser = context.GetService<IHtmlParser>();
+            var document = parser.ParseDocument(kinhDoanhPageContent);
+            var articles = document.QuerySelectorAll(selector);
+            foreach (var article in articles)
+            {
+                var anchors = article.QuerySelectorAll("a");
+                var link =  anchors[0].GetAttribute("href");
+                var title = anchors[0].InnerHtml;
+                var image = (IHtmlImageElement) anchors[2].QuerySelector("img");
+                var imageLink = image.Source;
+                var description = anchors[3].InnerHtml;
+                Console.WriteLine(title);
+                Console.WriteLine(imageLink);
+                Console.WriteLine(description);
+                await page.GoToAsync(link);
+                await page.WaitForNavigationAsync();
+                
+                
+                await page.GoBackAsync();
+                // foreach (var anchor in anchors)
+                // {
+                //     var unbox = (IHtmlAnchorElement) anchor;
+                //     unbox.h
+                //  Console.WriteLine("Begins:\n" + anchor.InnerHtml);   
+                // }
+            }
+        }                                                                                                                                                                                                                                                                                                                                        
+
+        private static async Task Navigate(Page page, string element)
+        {
+            var linkHandlers = await page.XPathAsync(element);
+
+            if (linkHandlers.Length > 0)
+            {
+                await linkHandlers[0].ClickAsync();
+                await page.WaitForNavigationAsync();
+                Console.WriteLine(page.Url);
+            }
+            else
+            {
+                throw new Exception("Link not found");
+            }
         }
 
         private static PageCreateModel CreatePageModel(string url, string pageContent)
@@ -41,20 +101,22 @@ namespace Crawler
             return page;
         }
 
-        private static async Task<string> GetPageContent(Browser browser, string url, int ViewPortWidth, int ViewPortHeight)
+        private static async Task<string> GetPageContent(Page page, string url, int ViewPortWidth, int ViewPortHeight)
         {
-            var page = await browser.NewPageAsync();
+            
             await page.SetViewportAsync(new ViewPortOptions
             {
                 Width  = ViewPortWidth,
                 Height = ViewPortHeight
             });
             await page.GoToAsync(url);
+            
             // var outputFile = "capture.jpg";
-//            await page.ScreenshotAsync(outputFile, new ScreenshotOptions {FullPage = true});
+            //            await page.ScreenshotAsync(outputFile, new ScreenshotOptions {FullPage = true});
             var pageContent = await page.GetContentAsync();
+            
             //Close tab page
-            await page.CloseAsync();
+            
             return pageContent;
         }
     }
